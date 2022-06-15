9cee02472c2cede449024557c7080d6b2114b74d
Gaurav Pandey <57361998+Gaurav3009@users.noreply.github.com>
2022-06-15 19:43:11 +0530

    dom event

--- document-object-model/app.js
+++ document-object-model/app.js
@@ -4,6 +4,39 @@ h1. addEventListener('click', ()=>{
     h1.style.color = "red";
 });
 
-const ul = document.querySelector('ul');
-const c = ul.children;
-console.log(c[1]);
+// const ul = document.querySelector('ul');
+// const c = ul.children;
+// console.log(c[1]); // selcts only the elememt nodes
+// console.log(ul.childNodes); // selects all the nodes including text nodes
+
+// console.log(ul.firstChild, ul.firstElementChild);
+
+// const liFirst = document.querySelector("li");
+// const p = liFirst.parentNode; // return the parent node 
+// console.log(p);
+
+// console.log(liFirst.closest('body'));
+
+// console.log(ul.previousSibling);// returns teh text nodes
+// console.log(ul.previousElementSibling); // returns header (element node)
+
+
+const section = document.querySelector("section");
+section.addEventListener("mouseover", ()=>{
+    section.style.backgroundColor = "blue";
+    section.style.cursor = "pointer";
+});
+section.addEventListener("mouseout", ()=>{
+    section.style.backgroundColor = "red";
+});
+
+const btn = document.querySelector("button");
+btn.addEventListener("click", ()=>{
+    // if(section.className == "red-bg visible"){
+    //     section.className = "red-bg invisible";
+    // }else{
+    //     section.className = "red-bg visible";
+    // }
+    // section.classList.toggle("visible");
+    section.classList.toggle("invisible");
+})
